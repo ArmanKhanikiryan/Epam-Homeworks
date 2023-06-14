924e9271a6d6589598c25e43a3937fc64ed2c90b
Arman Khanikiryan <akhanikiryan@gmail.com>
2023-06-15 03:41:42 +0400

    cluster csv parser done

--- multithreading/index.js
+++ multithreading/index.js
@@ -1,21 +1,22 @@
-import fs from 'fs';
-import csv from 'csv-parser';
+import fs from "fs";
+import csv from "csv-parser";
 
-process.on('message', message => {
-    const { csvFile, jsonFile } = message;
-    let count = 0;
-    fs.createReadStream(csvFile)
-        .pipe(csv())
-        .on('data', data => {
-            count++;
-        })
-        .on('end', () => {
-            fs.writeFile(jsonFile, JSON.stringify({ count }), err => {
-                if (err) {
-                    console.error(`Error writing to ${jsonFile}:`, err);
-                }
-                process.send({ count });
-                process.exit(0);
-            });
-        });
+process.on("message", (message) => {
+  const { csvFile, jsonFile } = message;
+  console.log(csvFile, 'CSV FILE ')
+  let count = 0;
+  fs.createReadStream(csvFile)
+    .pipe(csv())
+    .on("data", (data) => {
+      count++;
+    })
+    .on("end", () => {
+      fs.writeFile(jsonFile, JSON.stringify({ count }), (err) => {
+        if (err) {
+          console.error(`Error writing to ${jsonFile}:`, err);
+        }
+        process.send({ count });
+        process.exit(0);
+      });
+    });
 });
