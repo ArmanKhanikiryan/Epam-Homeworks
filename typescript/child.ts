56d0b7c60fa34c035c73eec6fcd05f6479fafd1f
ArmanKhanikiryan <akhanikiryan@gmail.com>
2023-06-28 00:13:31 +0400

    all typescript bugs are fixed

---
+++ typescript/child.ts
@@ -0,0 +1,79 @@
+import fs from 'fs';
+import { promises as fsPromises } from 'fs';
+import csv from 'csv-parser';
+import path from 'path';
+
+async function parserFunction(dirPath: string): Promise<void> {
+  const pathToConvert = './converted';
+  const csvDirectory = path.resolve(dirPath);
+
+  if (!csvDirectory) {
+    console.error('Invalid Directory Input');
+    process.exit(1);
+  }
+
+  try {
+    await fsPromises.access(csvDirectory);
+  } catch (err) {
+    console.error('Directory does not exist');
+    process.exit(1);
+  }
+
+  try {
+    await fsPromises.mkdir(pathToConvert);
+  } catch (err) {
+      console.error('Error creating the "converted" directory:', err);
+      process.exit(1);
+  }
+
+  try {
+    const files: string[] = await fsPromises.readdir(csvDirectory);
+    const csvFiles: string[] = files.filter((file) => path.extname(file) === '.csv');
+
+    if (!csvFiles.length) {
+      console.log('There are no .csv files in the directory');
+      process.exit(1);
+    }
+
+    for (const csvFile of csvFiles) {
+      const csvFilePath = path.join(csvDirectory, csvFile);
+      const jsonData: unknown = await parseCSVFile(csvFilePath);
+      const jsonFilePath: string = path.join(pathToConvert, `${path.parse(csvFile).name}.json`);
+      await writeJSONFile(jsonFilePath, jsonData);
+    }
+
+    console.log('Parsing completed');
+  } catch (err) {
+    console.error('Error during parsing:', err);
+    process.exit(1);
+  }
+}
+
+async function parseCSVFile(filePath: string) {
+  return new Promise((resolve, reject) => {
+    const jsonData: unknown[] = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on('data', data => {
+        jsonData.push(data);
+      })
+      .on('end', () => {
+        resolve(jsonData);
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
+}
+
+async function writeJSONFile(filePath: string, jsonData: unknown) {
+  return fsPromises.writeFile(filePath, JSON.stringify(jsonData, null, 2));
+}
+
+if (process.argv.length < 3) {
+  console.error('Invalid arguments. Usage: node parser.js <directory>');
+  process.exit(1);
+}
+
+const directory: string = process.argv[2];
+parserFunction(directory).then(r => console.log(r));
--- typescript/http.ts
+++ typescript/http.ts
@@ -1,85 +1,117 @@
-// import http from "http";
-// import path from "path";
-// import fsPromise from "fs/promises";
-// import url from "url";
-// import { UrlWithParsedQuery } from "url";
-// import { IncomingMessage, ServerResponse } from "http";
-//
-// const server = http.createServer(async (req: IncomingMessage, res:ServerResponse):Promise<void> => {
-//   const pathToConvert = '../clusters/converted';
-//   if (req !== undefined && req.url !== undefined){
-//     if (req.method === 'GET' && req.url === '/files'){
-//       const files:string[] = (await fsPromise.readdir(pathToConvert))
-//         .filter((file) => path.extname(file) === '.json');
-//       res.writeHead(200, { 'Content-Type': 'application/json' });
-//       res.end(JSON.stringify({ message: 'Get Request Succeeded', data: files }))
-//     }else if (req.method === 'POST' && req.url === '/exports') {
-//       let requestBody = ''
-//       req.on('data', (chunk) => {
-//           requestBody += chunk;
-//       });
-//       req.on('end', () => {
-//           let {dirPath} = JSON.parse(requestBody);
-//           dirPath = '../' + dirPath;
-//           console.log(dirPath);
-//           (async () => {
-//               try {
-//                   res.writeHead(201, { 'Content-Type': 'application/json' });
-//                   res.end(JSON.stringify({ message: 'Post Request Succeeded' }))
-//               }catch(err){
-//                   res.writeHead(400, { 'Content-Type': 'application/json' });
-//                   res.end(JSON.stringify({ message: 'Bad Request' }))
-//               }
-//           })()
-//       });
-//     }else if(req.method === 'GET' && req.url.startsWith('/files/')) {
-//       const reqUrl:UrlWithParsedQuery = url.parse(req.url, true);
-//       const filename:string = reqUrl.pathname!.substring('/files/:'.length);
-//       const ext:string = path.extname(filename)
-//       if (ext !== '.json'){
-//         res.writeHead(400, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({ message: 'Wrong File Type' }))
-//       }
-//       const fullPath:string = path.join(pathToConvert, filename)
-//       try {
-//         const result:Buffer = await fsPromise.readFile(fullPath)
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({ message: 'Get Request Succeeded', data: result }))
-//       }catch (e) {
-//         res.writeHead(404, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({ message: 'File Not Found' }))
-//       }
-//
-//     }else if (req.method === 'DELETE' && req.url.startsWith('/files/')){
-//       const parsedUrl:UrlWithParsedQuery = url.parse(req.url, true);
-//       const filename:string = parsedUrl.pathname!.substring('/files/:'.length);
-//       const ext:string = path.extname(filename)
-//       if (ext !== '.json'){
-//         res.writeHead(400, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({ message: 'Wrong File Type' }))
-//       }
-//       const fullPath:string = path.join('./', filename)
-//       console.log(fullPath)
-//       try{
-//         await fsPromise.rm(fullPath)
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({ message: 'Deleted' }))
-//       }catch(e){
-//         res.writeHead(404, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({ message: 'File Not Found' }))
-//       }
-//     }else{
-//       res.writeHead(404, { 'Content-Type': 'application/json' });
-//       res.end(JSON.stringify({ message: 'Not Found' }))
-//     }
-//   }
-// })
-// server.listen(9999)
-//
-//
-//
-//
-//
-//
-//
-//
+import http from "http";
+import path from "path";
+import fsPromise from "fs/promises";
+import url from "url";
+import { UrlWithParsedQuery } from "url";
+import { IncomingMessage, ServerResponse } from "http";
+import { spawn } from "child_process";
+
+interface ICors {
+  'Access-Control-Allow-Origin': string,
+  'Access-Control-Allow-Methods': string,
+  'Access-Control-Max-Age': number,
+}
+const pathToConvert = '../clusters/converted';
+const headers: ICors = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, DELETE',
+  'Access-Control-Max-Age': 2592000,
+};
+const server = http.createServer(async (req: IncomingMessage, res:ServerResponse):Promise<void> => {
+
+  if (req && req.url) {
+    if (req.method === 'GET' && req.url === '/files') {
+      await getAllFiles(req, res)
+    } else if (req.method === 'POST' && req.url === '/exports') {
+      await handlePost(req, res)
+    } else if (req.method === 'GET' && req.url.startsWith('/files/')) {
+      await getFileByFilename(req, res)
+    } else if (req.method === 'DELETE' && req.url.startsWith('/files/')) {
+      await handleDelete(req, res)
+    } else {
+      handleError(req, res)
+    }
+  }
+})
+
+
+async function getAllFiles(req: IncomingMessage, res:ServerResponse):Promise<void> {
+  const files:string[] = (await fsPromise.readdir(pathToConvert))
+    .filter((file) => path.extname(file) === '.json');
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message: 'Get Request Succeeded', data: files }))
+}
+
+async function handlePost(req: IncomingMessage, res:ServerResponse):Promise<void>{
+  let requestBody = '';
+  req.on('data', (chunk) => {
+    requestBody += chunk;
+  });
+  req.on('end', () => {
+    let { dirPath } = JSON.parse(requestBody);
+    dirPath = '../' + dirPath;
+    const parserProcess = spawn('node', ['../child-process/child.js', dirPath]);
+
+    parserProcess.stdout.on('data', (data) => {
+      console.log(`Parser output: ${data}`);
+    });
+
+    parserProcess.stderr.on('data', (data) => {
+      console.error(`Parser error: ${data}`);
+      res.writeHead(400, { 'Content-Type': 'application/json', ...headers });
+      res.end(JSON.stringify({ message: 'Bad Request' }))
+    });
+
+    parserProcess.on('close', (code) => {
+      console.log(`Parser process exited with code ${code}`);
+      res.writeHead(200, { 'Content-Type': 'application/json', ...headers });
+      res.end(JSON.stringify({ message: 'Post Request Succeeded' }))
+    });
+  });
+}
+
+async function getFileByFilename(req: IncomingMessage, res:ServerResponse):Promise<void>{
+  const reqUrl:UrlWithParsedQuery = url.parse(req.url!, true);
+  const filename:string = reqUrl.pathname!.substring('/files/:'.length);
+  const ext:string = path.extname(filename)
+  if (ext !== '.json'){
+    res.writeHead(400, { 'Content-Type': 'application/json', ...headers });
+    res.end(JSON.stringify({ message: 'Wrong File Type' }))
+  }
+  const fullPath:string = path.join(pathToConvert, filename)
+  try {
+    const result:Buffer = await fsPromise.readFile(fullPath)
+    res.writeHead(200, { 'Content-Type': 'application/json', ...headers });
+    res.end(JSON.stringify({ message: 'Get Request Succeeded', data: result }))
+  }catch (e) {
+    res.writeHead(404, { 'Content-Type': 'application/json', ...headers });
+    res.end(JSON.stringify({ message: 'File Not Found' }))
+  }
+}
+
+async function handleDelete(req: IncomingMessage, res:ServerResponse):Promise<void>{
+  const parsedUrl:UrlWithParsedQuery = url.parse(req.url!, true);
+  const filename:string = parsedUrl.pathname!.substring('/files/:'.length);
+  const ext:string = path.extname(filename)
+  if (ext !== '.json'){
+    res.writeHead(400, { 'Content-Type': 'application/json', ...headers });
+    res.end(JSON.stringify({ message: 'Wrong File Type' }))
+  }
+  const fullPath:string = path.join('./', filename)
+  console.log(fullPath)
+  try{
+    await fsPromise.rm(fullPath)
+    res.writeHead(200, { 'Content-Type': 'application/json', ...headers });
+    res.end(JSON.stringify({ message: 'Deleted' }))
+  }catch(e){
+    res.writeHead(404, { 'Content-Type': 'application/json', ...headers });
+    res.end(JSON.stringify({ message: 'File Not Found' }))
+  }
+}
+
+function handleError(req: IncomingMessage, res:ServerResponse):void{
+  res.writeHead(404, { 'Content-Type': 'application/json', ...headers });
+  res.end(JSON.stringify({ message: 'Not Found' }))
+}
+
+server.listen(9999)
